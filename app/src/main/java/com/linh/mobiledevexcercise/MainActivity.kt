6b0543af6e9ed47d413ca06ad173ef2acf3f5cab
Van Linh Nguyen <linhvnguyen9@gmail.com>
2021-03-13 20:12:29 +0700

    Initial commit

---
+++ app/src/main/java/com/linh/mobiledevexcercise/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.linh.mobiledevexcercise
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/linh/mobiledevexcercise/RegisterActivty.kt
@@ -0,0 +1,11 @@
+package com.linh.mobiledevexcercise
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivty : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_activty)
+    }
+}
